634af4f1a5aa9267545d42475d0312cf74c0be1b
stefanhelm <stefan.helm.99@icloud.com>
2020-12-06 12:57:37 +0100

    calc rsi and adding config

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ import numpy as np
 import websocket
 import json
 import pprint
+import talib
+import numpy
+from binance.client import Client
+from binance.enums import *
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
-closes =[]
+RSI_PERIOD = 14
+RSI_OVERBOUGHT = 70
+RSI_OVERSOLD = 30
+TRADE_SYMBOL = "ETHUSD"
+TRADE_QUANTITY = 0.05
 
+closes =[]
+in_position = False
+
+client = Client(config.API_KEY, config.API_Secret, tld="de")
 def on_open(ws):
     print('open connection')
 
@@ -14,6 +26,8 @@ def on_close(ws):
     print('close connection')
 
 def on_message(ws, message):
+    global closes
+
     print('received message')
     json_message = json.loads(message)
     pprint.pprint(json_message)
@@ -27,6 +41,29 @@ def on_message(ws, message):
         closes.append(float(close))
         print("closes")
         print(closes)
+
+        if len(closes) > RSI_PERIOD:
+            np_closes = numpy.array(closes)
+            rsi = talib.RSI(np_closes, RSI_PERIOD)
+            print("all rsis calculated so far")
+            print(rsi)
+            last_rsi = rsi[-1]
+            print("the current rsi is {}".format(last_rsi))
+
+            if last_rsi > RSI_OVERBOUGHT:
+                if in_position:
+                    print("Overbought! SELL! SELL! SELL!")
+                    # put binance sell logic here
+                else:
+                    print("It is overbought, but you don't own any. Nothing to do.")
+
+            if last_rsi < RSI_OVERSOLD:
+                if in_position:
+                    print("It is oversold, but you already own it, nothing  to do.")
+                else:
+                    print("Oversold! BUY! BUY! BUY")
+                    # out binance buy logic here
+
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 ws.run_forever()
